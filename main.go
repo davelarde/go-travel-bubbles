3477d9e212d95c26a0dc23e29105a3730456aae4
davelarde <danielavelarde4@gmail.com>
2022-12-06 13:57:34 -0500

    adding route handlers and endpoints

--- main.go
+++ main.go
@@ -9,3 +9,15 @@ import(
 	"strconv" 
 	"github.com/gorilla/mux"
 )  
+
+func main(){
+	// initializing mux router
+	r := mux.NewRouter() 
+
+	// Route handlers /end points
+	r.HandleFunc("/api/laundries", getLaundries).Methods("GET")
+	r.HandleFunc("/api/laundries/{id}", getLaundry).Methods("GET")
+	r.HandleFunc("/api/laundries", createLaundry).Methods("POST")
+	r.HandleFunc("/api/laundries/{id}", updateLaundry).Methods("PUT")
+	r.HandleFunc("/api/laundries/{id}", deleteLaundry).Methods("DELETE")
+}
