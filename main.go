97c13e9c22039b40cb48a5cb98ce678c9c64d2ef
davelarde <danielavelarde4@gmail.com>
2022-12-09 14:24:21 -0500

    succesful rendering in the welcome page, after filling out form, fixed data passing

--- main.go
+++ main.go
@@ -21,13 +21,13 @@ func init(){
 	ID string `json:id`
 	Laundrycode string `json:laundrycode`
 	Name string `json:name`
-	Location *Location `json:author`
- }
-//  location struct
-type Location struct{
 	Country string `json:country`
 	City string `json:city`
-}
+	// Location *Location `json:location`
+ }
+//  location struct
+// type Location struct{
+// }
 
 // init laundries var as alice Laundry struct
 var laundries []Laundry
@@ -39,15 +39,6 @@ func formPage(w http.ResponseWriter, r *http.Request){
     tpl.ExecuteTemplate(w, "form.html", nil)
 }
 
-//page for form
-// func formHandler(w http.ResponseWriter, r *http.Request){
-// 	if r.Method !="POST"{
-// 		http.Redirect(w, r, "/", http.StatusSeeOther)
-// 		return
-// 	}
-	
-
-// }
 // get all laundries
 func getLaundries(w http.ResponseWriter, r *http.Request){
     w.Header().Set("Content-Type", "application.json")
@@ -70,14 +61,45 @@ json.NewEncoder(w).Encode(&Laundry{})
 }
 
 func createLaundry(w http.ResponseWriter, r *http.Request){
-	w.Header().Set("Content-Type", "application.json")
-	var laundry Laundry
-	_ = json.NewDecoder(r.Body).Decode(&laundry)
-	laundry.ID = strconv.Itoa(rand.Intn(10000)) //mock id, not really used in production, use it only for test 
-	laundries = append(laundries, laundry)
-	json.NewEncoder(w).Encode(laundry)
+	
+	 if r.Method != "POST"{
+			tpl.Execute(w, nil)
+			return 
+		}
+		details := Laundry{
+	    Laundrycode : r.PostFormValue("laundryCode"),
+		Name : r.PostFormValue("laundryName"),
+		Country: r.PostFormValue("country"),
+		City: r.PostFormValue("city"),
+		
 
-}
+	
+	}
+		// fmt.Fprintf(w, "Name = %s\n",r.PostFormValue("laundryCode"))
+
+
+		// *Location : r.PostFormValue("country"),
+		// Location :r.PostFormValue("city"),
+	
+
+		tpl.ExecuteTemplate(w, "welcome.html", details)
+		w.Header().Set("Content-Type", "application.json")
+		var laundry Laundry
+		_ = json.NewDecoder(r.Body).Decode(&laundry)
+		laundry.ID = strconv.Itoa(rand.Intn(10000)) //mock id, not really used in production, use it only for test 
+		laundry.Name = details.Name
+		laundry.Laundrycode = details.Laundrycode
+		laundry.Country = details.Country
+		laundry.City = details.City
+
+		laundries = append(laundries, laundry)
+		json.NewEncoder(w).Encode(laundry)
+		
+	}
+	
+		
+	
+
 // Update a laundry
 func updateLaundry(w http.ResponseWriter, r *http.Request){
 	w.Header().Set("Content-Type", "application.json")
@@ -114,25 +136,25 @@ func main(){
 	// initializing mux router
 	r := mux.NewRouter() 
 // mock data
-laundries = append(laundries, Laundry{ID: "1", Laundrycode:"414141", Name: "Uphill Laundry", Location: &Location{Country: "USA", City:"San Francisco"}})
+laundries = append(laundries, Laundry{ID: "1", Laundrycode:"414141", Name: "Uphill Laundry",Country: "USA", City:"San Francisco"})
 
-laundries = append(laundries, Laundry{ID: "2", Laundrycode:"000001", Name: "Laundry Nowhere", Location: &Location{Country: "Chile", City:"Arica"}})
+laundries = append(laundries, Laundry{ID: "2", Laundrycode:"000001", Name: "Laundry Nowhere",Country: "Chile", City:"Arica"})
 
-laundries = append(laundries, Laundry{ID: "3", Laundrycode:"123456", Name: "Lavanderia Castillo", Location: &Location{Country: "Spain", City:"Madrid"}})
+laundries = append(laundries, Laundry{ID: "3", Laundrycode:"123456", Name: "Lavanderia Castillo", Country: "Spain", City:"Madrid"})
 
-laundries = append(laundries, Laundry{ID: "4", Laundrycode:"245678", Name: "Ciao Laundry!", Location: &Location{Country: "Italy", City:"Aosta"}})
+laundries = append(laundries, Laundry{ID: "4", Laundrycode:"245678", Name: "Ciao Laundry!", Country: "Italy", City:"Aosta"})
 
-laundries = append(laundries, Laundry{ID: "5", Laundrycode:"908756", Name: "Aussie Mat!", Location: &Location{Country: "Australia", City:"Melbourne"}})
+laundries = append(laundries, Laundry{ID: "5", Laundrycode:"908756", Name: "Aussie Mat!", Country: "Australia", City:"Melbourne"})
 
-laundries = append(laundries, Laundry{ID: "6", Laundrycode:"345677", Name: "Vetements propes", Location: &Location{Country: "France", City:"Bordeaux"}})
+laundries = append(laundries, Laundry{ID: "6", Laundrycode:"345677", Name: "Vetements propes", Country: "France", City:"Bordeaux"})
 
-laundries = append(laundries, Laundry{ID: "7", Laundrycode:"745678", Name: "kabarciklar", Location: &Location{Country: "Turkey", City:"Istanbul"}})
+laundries = append(laundries, Laundry{ID: "7", Laundrycode:"745678", Name: "kabarciklar", Country: "Turkey", City:"Istanbul"})
 
-laundries = append(laundries, Laundry{ID: "8", Laundrycode:"345678", Name: "Easy laundry", Location: &Location{Country: "USA", City:"Philadelphia"}})
+laundries = append(laundries, Laundry{ID: "8", Laundrycode:"345678", Name: "Easy laundry", Country: "USA", City:"Philadelphia"})
 
-laundries = append(laundries, Laundry{ID: "9", Laundrycode:"202204", Name: "Lavanderia Rapida", Location: &Location{Country: "Brazil", City:"Salvador do Bahia"}})
+laundries = append(laundries, Laundry{ID: "9", Laundrycode:"202204", Name: "Lavanderia Rapida", Country: "Brazil", City:"Salvador do Bahia"})
 
-laundries = append(laundries, Laundry{ID: "10", Laundrycode:"444444", Name: "Aguayo", Location: &Location{Country: "Bolivia", City:"La Paz"}})
+laundries = append(laundries, Laundry{ID: "10", Laundrycode:"444444", Name: "Aguayo", Country: "Bolivia", City:"La Paz"})
 
 	// Route handlers /end points
     // fs:= http.FileServer(http.Dir("./assets/"))
@@ -141,10 +163,10 @@ laundries = append(laundries, Laundry{ID: "10", Laundrycode:"444444", Name: "Agu
 
 	r.HandleFunc("/", laundryPage)
 	r.HandleFunc("/form", formPage)
-	// r.HandleFunc("/form", formHandler)
+	// r.HandleFunc("/welcome", formHandler)
 	r.HandleFunc("/laundries", getLaundries).Methods("GET")
 	r.HandleFunc("/laundries/{id}", getLaundry).Methods("GET")
-	r.HandleFunc("/laundries", createLaundry).Methods("POST")
+	r.HandleFunc("/welcome", createLaundry).Methods("POST")
 	r.HandleFunc("/laundries/{id}", updateLaundry).Methods("PUT")
 	r.HandleFunc("/laundries/{id}", deleteLaundry).Methods("DELETE")
 	log.Fatal(http.ListenAndServe(":8000", r))
